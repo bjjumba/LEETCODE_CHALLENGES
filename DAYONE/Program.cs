11b145444890705e20a6a8431b4529fc286c5e39
bjjumba <ericjjumba5@gmail.com>
2023-10-05 17:20:12 +0300

    First Commit

---
+++ DAYONE/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic; 
+
+namespace DayOne;
+
+public class Program {
+
+    public static IList<int> MajorityElement(int[] nums) {
+        /*Size of Array*/
+        Dictionary <int, int> number_freq = new Dictionary<int,int>();
+        IList<int> magic = new List<int>();
+        
+        int size = nums.Length;
+        int n = size/3;
+
+        foreach (var num in nums)
+        {
+            if(number_freq.ContainsKey(num)) {
+                number_freq[num]++;
+            } else {
+                number_freq[num] = 1;
+            }
+        }
+        /* Iterate through the frequency */
+        foreach (var number in number_freq)
+        {  
+            if (number.Value > n)
+            {
+                magic.Add(number.Key);
+            }
+        }
+        return magic;
+    }
+    static void Main(){
+        int[] nums = {1, 2, 3, 3};
+        var response = MajorityElement(nums);
+    }
+}
